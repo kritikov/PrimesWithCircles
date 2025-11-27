a899c0559de7ab472d01399a8d677314bb5a382a
kritikov <nikoskritikou@gmail.com>
2025-11-27 18:54:06 +0200

    working

--- PrimesWithCircles/MainWindow.xaml.cs
+++ PrimesWithCircles/MainWindow.xaml.cs
@@ -11,12 +11,13 @@ namespace PrimesWithCircles
     public partial class MainWindow : Window
     {
         private List<Circle> circles = new List<Circle>();
-        private double pointersSpeed = 0.1;
+        private double rotationSpeed = 0.1;
         private double firstRadius = 10;
         private Point screenCenter;
 
 
-        private int lapCounter = 0;
+        private int lapCounter = 2;
+        const double finishLine = 3 * Math.PI / 2;
         private readonly DispatcherTimer timer = new();
 
         private bool isStepping = false;
@@ -39,14 +40,12 @@ namespace PrimesWithCircles
 
             timer.Interval = TimeSpan.FromMilliseconds(16);
             timer.Tick += OnTick;
-            timer.Start();
         }
 
 
         /// <summary>
         /// Add a circle to the list
         /// </summary>
-        /// <param name="radious"></param>
         private void AddCircle(double radious)
         {
             var circle = new Circle(radious);
@@ -62,7 +61,6 @@ namespace PrimesWithCircles
         /// <summary>
         ///  Position a circle in the center of the screen
         /// </summary>
-        /// <param name="circle"></param>
         private void CenterCircle(Circle circle)
         {
             // position the circle in the center of the screen
@@ -74,8 +72,6 @@ namespace PrimesWithCircles
         /// <summary>
         ///  Position the pointer of a circle to a specific angle
         /// </summary>
-        /// <param name="circle"></param>
-        /// <param name="angle"></param>
         private void PositionPointer(Circle circle)
         {
             // position the pointer on top of the circle
@@ -86,109 +82,74 @@ namespace PrimesWithCircles
         }
 
         /// <summary>
+        ///  Rotate all circles on each timer tick
+        /// </summary>
+        private void OnTick(object sender, EventArgs e)
+        {
+            RotateCircles();
+        }
+
+        /// <summary>
         /// Rotate all the circles
         /// </summary>
         private void RotateCircles()
         {
-            foreach (var circle in circles)
+            bool firstCircleCompletedLap = false;
+            bool someOtherCircleCompletedLap = false;
+
+            // foreach: περιστροφή όλων
+            for (int i = 0; i < circles.Count; i++)
             {
-                RotateCircle(circle);
+                bool lapCompleted = RotateCircle(circles[i]);
+
+                // ΜΟΝΟ ο πρώτος μάς ενδιαφέρει
+                if (i == 0 && lapCompleted)
+                    firstCircleCompletedLap = true;
+                else if (lapCompleted)
+                    someOtherCircleCompletedLap = true;
+            }
+
+            // if the first circle completed a lap, stop the timer
+            if (firstCircleCompletedLap)
+            {
+                timer.Stop();
+                lapCounter++;
+                LapCounterText.Text = lapCounter.ToString();
+
+                // if no other circle completed a lap, add a new circle
+                if (!someOtherCircleCompletedLap)
+                {
+                    AddCircle(firstRadius * (lapCounter));
+                }
             }
         }
 
         /// <summary>
         /// Rotate a circle
         /// </summary>
-        /// <param name="circle"></param>
-        private void RotateCircle(Circle circle)
+        private bool RotateCircle(Circle circle)
         {
-            circle.Angle += pointersSpeed / (circle.Radious / firstRadius);
-            if (circle.Angle >= Math.PI * 2)
+            double prev = circle.Angle;
+
+            // increment
+            circle.Angle += rotationSpeed / (circle.Radious / firstRadius);
+
+            // detect lap BEFORE wrap
+            bool completedLap = circle.Angle >= (3 * Math.PI / 2);
+
+            if (completedLap)
                 circle.Angle -= Math.PI * 2;
 
             PositionPointer(circle);
+
+            return completedLap;
         }
 
-        
-
-        
-        private void OnTick(object sender, EventArgs e)
-        {
-            RotateCircles();
-            //if (AutoModeCheck.IsChecked == true)
-            //{
-            //    RotateNormally();
-            //}
-            //else
-            //{
-            //    // Manual mode: only rotate if a step cycle is active
-            //    if (isStepping)
-            //    {
-            //        StepRotate();
-            //    }
-            //}
-        }
-
-        private void StepRotate()
-        {
-            //double prev = angle;
-
-            //angle += 0.1;
-            //if (angle >= Math.PI * 2)
-            //    angle -= Math.PI * 2;
-
-            //UpdateDotPositions();
-
-            //// Αν έχουμε ξεπεράσει την targetAngle -> ένας πλήρης κύκλος ολοκληρώθηκε
-            //double normalizedAngle = angle < startAngle ? angle + 2 * Math.PI : angle;
-
-            //if (normalizedAngle >= targetAngle)
-            //{
-            //    // "Κουμπώνουμε" την τελεία ακριβώς στη θέση τερματισμού
-            //    angle = startAngle;
-            //    isStepping = false;
-            //}
-        }
-
-
-        private void DetectLap(double prev)
-        {
-            //double finishLine = 3 * Math.PI / 2;
-
-            //if (prev < finishLine && angle >= finishLine)
-            //{
-            //    lapCounter++;
-            //    LapCounterText.Text = lapCounter.ToString();
-            //}
-        }
-
-        private void UpdateDotPositions()
-        {
-            //double x1 = screenCenter.X + firstRadius * Math.Cos(angle);
-            //double y1 = screenCenter.Y + firstRadius * Math.Sin(angle);
-
-            //Canvas.SetLeft(dot, x1 - dot.Width / 2);
-            //Canvas.SetTop(dot, y1 - dot.Height / 2);
-
-            //double x2 = screenCenter.X + radius2 * Math.Cos(angle / 2);
-            //double y2 = screenCenter.Y + radius2 * Math.Sin(angle / 2);
-
-            //Canvas.SetLeft(dot2, x2 - dot2.Width / 2);
-            //Canvas.SetTop(dot2, y2 - dot2.Height / 2);
-        }
 
         // Called when user clicks "Step"
         private void StepButton_Click(object sender, RoutedEventArgs e)
         {
-            //if (AutoModeCheck.IsChecked == true)
-            //    return;
-
-            //if (!isStepping)
-            //{
-            //    isStepping = true;
-            //    startAngle = angle;
-            //    targetAngle = angle + 2 * Math.PI;
-            //}
+            timer.Start();
         }
     }
 }
